e248f3bb0c95295e399944d89169cfbef6e34429
Nick Belzer <me@nickbelzer.me>
2021-06-28 15:23:22 +0200

    Different updates
    Due to a re-install of my computer I am merging all these changes into
    one commit.

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -6,3 +6,17 @@
 
 ;; Do not resize the frame at this early stage.
 (setq frame-inhibit-implied-resize t)
+
+;; Disable GUI elements
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-splash-screen t)
+(setq use-dialog-box t)                 ; only for mouse events
+(setq use-file-dialog nil)
+
+(setq inhibit-startup-echo-area-message "prot") ; read the docstring
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-buffer-menu t)
+
+(setq native-comp-async-report-warnings-errors 'silent) ; emacs28 with native compilation
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,6 +6,9 @@
 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+
+
 
 ;; Initialise the packages, avoiding a re-initialisation.
 (unless (bound-and-true-p package--initialized)
@@ -19,7 +22,7 @@
 
 ;; Configure `use-package' prior to loading it.
 (eval-and-compile
-  (setq use-package-always-ensure nil)
+  (setq use-package-always-ensure t)
   (setq use-package-always-defer nil)
   (setq use-package-always-demand nil)
   (setq use-package-expand-minimally nil)
@@ -34,14 +37,59 @@
   :config
   (setq vc-follow-symlinks t)) ; Because my dotfiles are managed that way
 
-(use-package org)
+(use-package org
+  :ensure t)
+
+;; Some basic settings
+(setq frame-title-format '("%b"))
+(setq ring-bell-function 'ignore)
+(setq use-short-answers t)
 
 (let* ((conf "~/.emacs.d/emacs-init")
-       (el (concat conf ".el"))
+       (el (concat conf ".elc"))
        (org (concat conf ".org")))
-  (if (file-exists-p el)
+  (if (or (file-exists-p el) (file-symlink-p el))
       (load-file el)
-    (org-babel-load-file org)))
+    (progn (org-babel-load-file org)
+	       (message "falling back on org-babel-load-file for init"))))
 
 ;; Make GC pauses faster by decreasing the threshold.
 (setq gc-cons-threshold (* 2 1000 1000))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bibtex-autokey-name-year-separator "")
+ '(bibtex-autokey-titleword-separator "-")
+ '(bibtex-autokey-titlewords 2)
+ '(bibtex-autokey-titlewords-stretch 1)
+ '(bibtex-autokey-year-title-separator "-")
+ '(company-idle-delay 0.0)
+ '(company-minimum-prefix-length 1)
+ '(company-posframe-mode t)
+ '(company-tooltip-limit 5)
+ '(custom-safe-themes
+   '("7ea491e912d419e6d4be9a339876293fff5c8d13f6e84e9f75388063b5f794d6" default))
+ '(latex-run-command "pdflatex")
+ '(lsp-solargraph-autoformat t)
+ '(markdown-enable-math t)
+ '(markdown-hide-markup t)
+ '(mode-line-format
+   '("%e" mode-line-front-space mode-line-mule-info mode-line-client mode-line-modified mode-line-remote mode-line-frame-identification mode-line-buffer-identification "   " mode-line-position
+     (vc-mode vc-mode)
+     "  " mode-line-misc-info mode-line-end-spaces))
+ '(ns-command-modifier 'control)
+ '(ns-control-modifier 'meta)
+ '(org-download-image-org-width 300)
+ '(org-download-screenshot-method "screencapture -i %s")
+ '(package-selected-packages
+   '(ob-racket dired window ac-emoji diff-hl vue-mode ob-lisp org-src slime ruby-electric lsp-python-ms ruby-test-mode diminish company-box eglot elfeed hide-mode-line org-present lsp-ivy typescript-mode svelte-mode org-ref ivy-bibtex bibtex-completion pdf-tools org-roam org-plus-contrib org-superstar yaml-mode dockerfile-mode graphql-mode racket-mode olivetti yasnippet which-key vterm use-package swift-mode pyvenv modus-vivendi-theme modus-operandi-theme magit lsp-sourcekit ivy-rich ivy-prescient go-mode flycheck-posframe flycheck-indicator exec-path-from-shell dap-mode counsel bufler beacon))
+ '(warning-suppress-types '((comp) (:warning)))
+ '(web-mode-block-padding -1))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
