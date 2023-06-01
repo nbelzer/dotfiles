dafe8a57b8cba7486ad49bae09bb50dd81c0656f
Nick Belzer <me@nickbelzer.me>
2023-06-01 11:00:28 +0200

    Add straight.el to init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -33,6 +33,23 @@
 (eval-when-compile
   (require 'use-package))
 
+; Install straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+; Setup the straight integration.
+(straight-use-package 'use-package)
+
 (use-package vc
   :config
   (setq vc-follow-symlinks t)) ; Because my dotfiles are managed that way
